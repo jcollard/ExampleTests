3d71777cc2b8f64d17a94d018fc13c7a3b2a587e
Joseph Collard <jcollard@chadwickschool.org>
2022-02-03 11:35:40 -0800

    chore: Add examples from class.

--- TestGetPositiveInteger.cs
+++ TestGetPositiveInteger.cs
@@ -7,7 +7,22 @@ namespace ExampleTests
 
         public static bool RunTest()
         {
-            return false;
+            
+            Console.WriteLine("Testing GetPositiveInteger.");
+            Console.WriteLine("You should expect to see the message 'How old are you?'");
+            Console.WriteLine("1. Enter '-5'. This should display 'Invalid.' and ask again.");
+            Console.WriteLine("2. Enter '0'. This should display 'Invalid' and ask again.");
+            Console.WriteLine("3. Just press enter (do not enter anything). This should display 'Invalid' and ask again.");
+            Console.WriteLine("4. Enter '17'. This should be successful.");
+            int testResult = Program.GetPositiveInteger("How old are you?");
+            int expected = 17;
+            if (testResult != expected)
+            {
+                Console.Error.WriteLine($"Failure: Expected 17 but the result was {testResult}.");
+                return false;
+            }
+
+            return true;
         }
     }
 }
--- TestIsPositiveInteger.cs
+++ TestIsPositiveInteger.cs
@@ -7,7 +7,49 @@ namespace ExampleTests
 
         public static bool RunTest()
         {
-            return false;
+            string testInput = "5";
+            bool testResult = Program.IsPositiveInteger(testInput);
+            bool expected = true;
+            if (testResult != expected)
+            {
+                Console.Error.WriteLine($"Failure: Expected true but was actually {testResult}");
+                return false;
+            }
+
+            string testInput2 = "-2";
+            bool testResult2 = Program.IsPositiveInteger(testInput2);
+            bool expected2 = false;
+
+            if (testResult2 != expected2)
+            {
+                Console.Error.WriteLine($"Failure: Expected false but was actually {testResult2}");
+                return false;
+            }
+
+            testInput = "3.5";
+            expected = false;
+            testResult = Program.IsPositiveInteger(testInput);
+
+            if (testResult != expected)
+            {
+                Console.Error.WriteLine($"Failure: Expected true but was actually {testResult}");
+                return false;
+            }
+
+            try
+            {
+                testResult = Program.IsPositiveInteger(null);
+
+                Console.Error.WriteLine("Failure: Expected the program to crash. But it did not.");
+                return false;
+            }
+            catch
+            {
+
+            }
+
+            // Our method seems to be working
+            return true;
         }
     }
 }
