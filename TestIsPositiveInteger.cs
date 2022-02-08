f17d948c6c1238264ad97e301c61f0b15569830c
Joseph Collard <jcollard@chadwickschool.org>
2022-02-08 09:02:30 -0800

    chore: Update test.

--- TestIsPositiveInteger.cs
+++ TestIsPositiveInteger.cs
@@ -7,8 +7,36 @@ namespace ExampleTests
 
         public static bool RunTest()
         {
-            
-            return false;
+            bool result = Program.IsPositiveInteger("hashtag");
+            bool expected = false;
+
+            if (result != expected)
+            {
+                Console.Error.WriteLine($"Failure: Expected {expected} but the result was {result}");
+                return false;
+            }
+
+            try 
+            {
+                Program.IsPositiveInteger(null);
+                Console.Error.WriteLine($"Failure: Expected an exception but there was none.");
+                return false;
+            }
+            catch 
+            {
+
+            }
+
+            result = Program.IsPositiveInteger("2");
+            expected = true;
+
+            if (result != expected)
+            {
+                Console.Error.WriteLine($"Failure: Expected {expected} but the result was {result}");
+                return false;
+            }
+
+            return true;
         }
     }
 }
